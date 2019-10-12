334ff46d0dbbd3a1d016c6d0c33f93835eae2f0d
pablozabala <54216099+pablozabala@users.noreply.github.com>
2019-10-12 14:16:21 -0300

    reegghhgeeerrrdfdfd

--- WindowsFormsGIT/WindowsFormsGIT/Form1.Designer.cs
+++ WindowsFormsGIT/WindowsFormsGIT/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(139, 61);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(93, 56);
             this.button1.TabIndex = 0;
             this.button1.Text = "HOME";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,6 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
