3aafadba141958cbfc9b9f341769fcb3a80b2eaa
pablozabala <pablozabala.jakemate@gmail.com>
2019-10-18 16:01:36 -0300

    ghghgh

--- WindowsFormsGIT/WindowsFormsGIT/Form3.cs
+++ WindowsFormsGIT/WindowsFormsGIT/Form3.cs
@@ -19,34 +19,58 @@ namespace WindowsFormsGIT
 
         private void Form3_Load(object sender, EventArgs e)
         {
+            Randown();
             int Monto = 7500;
             int c = 0;
             int expensa = 4000;
             int[] vector = new int[24];
             int total = 0;
-            for (int i=0;i<24;i++)
+            for (int i = 0; i < 24; i++)
             {
                 vector[i] = Monto;
                 c++;
-                if (c ==6)
+                if (c == 6)
                 {
                     Monto = Monto + Monto * 15 / 100;
                     c = 0;
                 }
                 total = total + Monto + expensa;
             }
-            string msj = "Total pagado " + Monto.ToString ();
+            string msj = "Total pagado " + Monto.ToString();
             MessageBox.Show(msj);
         }
 
-        private int vectores()
-        {
-                        
-        }
+       
+
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Randown()
+        {
+            Random r = new Random();
+            int[] vec = new int[37];
+            for (int i = 0; i < vec.Length; i++)
+            {
+                vec[i] = 0;
+            }
+            int c = 0;
+            int numero = 0;
+            while (c < 1000)
+            {
+                numero = r.Next(0, 37);
+                vec[numero] = vec[numero] + 1;
+                c++;
+            }
+            string msj = "";
+            for (int i = 0; i < vec.Length; i++)
+            {
+                msj = "numero " + i.ToString();
+                msj = msj + " CANTIDAD " + vec[i].ToString();
+                MessageBox.Show(msj);
+            }
         }
     }
 }
--- WindowsFormsGIT/WindowsFormsGIT/Program.cs
+++ WindowsFormsGIT/WindowsFormsGIT/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsGIT
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form3());
         }
     }
 }
